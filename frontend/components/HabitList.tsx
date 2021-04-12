c1394b89506a4303e4d0fef2fdf1e365bfdd34cb
Alexander Jesinkey <alexander.jesinkey@gmail.com>
2021-04-12 20:39:27 +0200

    Move mock habit list to own file

--- frontend/components/HabitList.tsx
+++ frontend/components/HabitList.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
 
 import Habit from './Habit';
+import { HABIT_LIST_MOCK } from '../mock/HabitMockData';
 
 export default function HabitList() {
-  const habits = [
-    {
-      id: 1,
-      name: 'Habit 1',
-    },
-    {
-      id: 2,
-      name: 'Habit 2',
-    },
-    {
-      id: 3,
-      name: 'Habit 3',
-    },
-  ];
   return (
     <>
-      {habits.map((habit) => {
+      {HABIT_LIST_MOCK.map((habit) => {
         return <Habit key={habit.id} habit={habit} />;
       })}
     </>
---
+++ frontend/mock/HabitMockData.tsx
@@ -0,0 +1,14 @@
+export const HABIT_LIST_MOCK = [
+  {
+    id: 1,
+    name: 'Habit 1',
+  },
+  {
+    id: 2,
+    name: 'Habit 2',
+  },
+  {
+    id: 3,
+    name: 'Habit 3',
+  },
+];
